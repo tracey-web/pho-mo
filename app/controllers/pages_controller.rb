3e84144e46d6876eb50c453414836f2773be40a8
Tracey <traceylea9@gmail.com>
2021-10-29 00:12:52 +1100

    fixed login/page error

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,6 @@
 class PagesController < ApplicationController
   def home
+    check_for_login
     @posts = Post.order(created_at: :desc)
     @users = User.where.not(id: @current_user.id).limit(5)
   end
